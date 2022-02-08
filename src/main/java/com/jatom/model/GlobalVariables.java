2e67d53e68cfc42e8891be80cec33c520a1cccb2
Geovane Araújo <geovane-araujo@outlook.com>
2022-02-08 07:56:51 -0300

    correção da desserialização gson

--- src/main/java/com/jatom/model/GlobalVariables.java
+++ src/main/java/com/jatom/model/GlobalVariables.java
@@ -1,6 +1,8 @@
 package com.jatom.model;
 
+import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.jatom.ConnectionDatabase;
 
 import java.sql.Connection;
@@ -12,7 +14,7 @@ public class GlobalVariables {
     protected Connection con = null;
     protected Boolean collect = false;
     protected PreparedStatement stmt = null;
-    protected Gson gson = new Gson();
+    protected Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.IDENTITY).create();
     protected ResultSet rs = null;
     protected ConnectionDatabase connectionDatabase = new ConnectionDatabase();
 }
